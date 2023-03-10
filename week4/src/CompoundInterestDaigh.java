8993d084c59b5d4362382ae471e4b4ee7b79ffde
Deon Daigh <deon.daigh@gmail.com>
2023-03-09 19:26:00 -0600

    Week 4
    Added unit tests to CompoundInterestDaigh and WidgetOrderDaigh

---
+++ week4/src/CompoundInterestDaigh.java
@@ -0,0 +1,21 @@
+
+/**  
+* Deon Daigh - dmdaigh
+* CIS171 23355
+* Mar 5, 2023
+* MacOS 13.2
+*/
+public class CompoundInterestDaigh {
+	public static double computeBalance(double initialDeposit, double interestRate, int numOfYears) {
+//		Calculates the future value based on the arguments given
+		double futureValue = initialDeposit * Math.pow( 1 + interestRate, numOfYears);
+		return futureValue; // returns the future value
+	}
+
+	public static void main(String[] args) {
+		System.out.printf("Your total is $%.2f", computeBalance(10000, .045, 13));
+		System.out.printf("\nYour total is $%.2f", computeBalance(20000, .03, 15));
+		System.out.printf("\nYour total is $%.2f", computeBalance(30000, .01, 20));
+	}
+
+}
